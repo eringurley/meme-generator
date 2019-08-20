f678284f4204e5687c1930490b0ed4f144f126ce
Erin Gurley <erinmgurley@gmail.com>
2019-08-20 16:11:46 -0700

    rendering components in app

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
+import MemeInput from './MemeInput';
+import MemeDisplay from './MemeDisplay';
 
-export default function App() {
-  return <h1>Hello World</h1>;
+export default class App extends Component {
+  state = {
+    header:'',
+    imgURL:'',
+    footer:''
+  };
+
+  handleChange= ({ target }) => {
+    this.setState({ [target.name]: target.value });
+  }
+
+  render() {
+    const { header, imgURL, footer } = this.state;
+    return (
+    <>
+      <h1>Meme-orizer</h1>
+      <MemeInput header={header} imgURL={imgURL} footer={footer} handleChange={this.handleChange}/>
+      <MemeDisplay top={header} imgURL={imgURL} footer={footer}/>
+    </>
+    );
+  }
 }
-  
